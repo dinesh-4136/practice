b41932c40e63f84c3644d47466fec551ec4d11b9
dinesh-4136 <r.dinesh4136@gmail.com>
2026-02-27 17:18:16 +0530

    backend block updated

--- terraform/ec2-by-tf/backend.tf
+++ terraform/ec2-by-tf/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "tf-s3-bkt-state-backup-a1"
+    bucket  = "tf-s3-bkt-state-backup-a2"
     key     = "backend/terraform.tfstate"
     region  = "ap-south-1"
     encrypt = true
